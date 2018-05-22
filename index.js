faa393a2b4b9d9e4b817aa52f02fc6938dd19640
Mason <me@masonx.ca>
2018-05-21 20:10:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,55 @@
-"use strict";
+const http = require('http');
 
 var Service, Characteristic, HomebridgeAPI;
 
 module.exports = function(homebridge) {
-
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   HomebridgeAPI = homebridge;
-  homebridge.registerAccessory("homebridge-dummy", "DummySwitch", DummySwitch);
+  homebridge.registerAccessory("homebridge-gpio-toggle", "GpioToggle", GpioToggle);
 }
 
-function DummySwitch(log, config) {
+function GpioToggle(log, config) {
   this.log = log;
   this.name = config.name;
-  this.stateful = config.stateful;
-  this.reverse = config.reverse;
+  this.pin = config.pin;
+  this.api = config.api;
+  this.length = config.length;
   this._service = new Service.Switch(this.name);
-  
-  this.cacheDirectory = HomebridgeAPI.user.persistPath();
-  this.storage = require('node-persist');
-  this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
   
   this._service.getCharacteristic(Characteristic.On)
     .on('set', this._setOn.bind(this));
-
-  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
-
-  if (this.stateful) {
-	var cachedState = this.storage.getItemSync(this.name);
-	if((cachedState === undefined) || (cachedState === false)) {
-		this._service.setCharacteristic(Characteristic.On, false);
-	} else {
-		this._service.setCharacteristic(Characteristic.On, true);
-	}
-  }
 }
 
-DummySwitch.prototype.getServices = function() {
+GpioToggle.prototype.getServices = function() {
   return [this._service];
 }
 
-DummySwitch.prototype._setOn = function(on, callback) {
+GpioToggle.prototype._setOn = function(on, callback) {
+  // An object of options to indicate where to post to
+  this.log("Toggling switch on momentarily!");
+  var request_opts = {
+      host: this.api,
+      port: '8080',
+      path: '/api/gpio/' + this.pin,
+      method: 'PATCH',
+      headers: {
+          'Content-Type': 'text/plain',
+          'Content-Length': Buffer.byteLength(this.length)
+      }
+  };
 
-  this.log("Setting switch to " + on);
+  // Set up the request
+  var request = http.request(request_opts, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        this._service.setCharacteristic(Characteristic.On, false);
+    });
+  });
 
-  if (on && !this.reverse && !this.stateful) {
-    setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, false);
-    }.bind(this), 1000);
-  } else if (!on && this.reverse && !this.stateful) {
-    setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, true);
-    }.bind(this), 1000);
-  }
-  
-  if (this.stateful) {
-	this.storage.setItemSync(this.name, on);
-  }
-  
+  // post the data
+  request.write(this.length);
+  request.end();
+
   callback();
 }
